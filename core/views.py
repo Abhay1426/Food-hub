8227781672fbeb526c3a06d37e1ddbbc102b86ae
Abhay1426 <abhayjsr831@gmail.com>
2026-05-08 14:04:59 +0530

    Added

--- core/views.py
+++ core/views.py
@@ -1,3 +1,5 @@
+import os
+
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
@@ -18,8 +20,8 @@ import razorpay
 import json
 
 # Razorpay client
-RAZORPAY_KEY_ID = "rzp_test_SbeuOROXnHQDuz"
-RAZORPAY_KEY_SECRET = "lnJwqU1kOXSLYh4091mKbpNk"
+RAZORPAY_KEY_ID = os.getenv('RAZORPAY_KEY_ID')
+RAZORPAY_KEY_SECRET = os.getenv('RAZORPAY_KEY_SECRET')
 razorpay_client = razorpay.Client(auth=(RAZORPAY_KEY_ID, RAZORPAY_KEY_SECRET))
 
 
